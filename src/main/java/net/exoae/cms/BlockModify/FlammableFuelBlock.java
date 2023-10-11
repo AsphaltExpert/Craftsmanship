a87c7ac300b7591da626afe3e97029b209a19447
AsphaltExpert <anton_stauber@gmx.de>
2023-10-11 21:33:16 +0200

    Updated

---
+++ src/main/java/net/exoae/cms/BlockModify/FlammableFuelBlock.java
@@ -0,0 +1,47 @@
+package net.exoae.cms.BlockModify;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+import javax.annotation.Nullable;
+
+public class FlammableFuelBlock {
+
+    public class FlammableBlock extends Block {
+        public FlammableBlock (Properties p_49795_) {
+            super(p_49795_);
+        }
+        @Override
+        public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+            return true;
+        }
+        @Override
+        public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+            return 10;
+        }
+
+        @Override
+        public int getFireSpreadSpeed (BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+            return 5;
+        }
+    }
+    public class FuelBlock extends BlockItem {
+        private int burnTime = 0;
+
+        public FuelBlock(Properties pProperties, int burnTime) {
+            super(pProperties);
+            this.burnTime = burnTime;
+        }
+
+        @Override
+        public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+            return this.burnTime;
+        }
+    }
+}
